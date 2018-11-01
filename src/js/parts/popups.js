25b1e3b40efbed7c9db3d11600fc324cf7d0b62e
FaZY <FaZY@mail.ru>
2018-11-01 14:33:45 +0300

    Модальное окно с подарком

--- src/js/parts/popups.js
+++ src/js/parts/popups.js
@@ -1,8 +1,8 @@
 function popups() {
     function popupDesign() {
         let btns = document.querySelectorAll('.button-design'),
-        overlay = document.querySelector('.popup-design'),
-        close = overlay.querySelector('.popup-close');
+            overlay = document.querySelector('.popup-design'),
+            close = overlay.querySelector('.popup-close');
     
         for (let i = 0; i < btns.length; i++) {
             btns[i].addEventListener('click', function() {
@@ -31,11 +31,11 @@ function popups() {
     
     popupDesign();
     
-    // button-consultation
+    // popup-consultation
     function popupConsultation() {
         let btns = document.querySelectorAll('.button-consultation'),
-        overlay = document.querySelector('.popup-consultation'),
-        close = overlay.querySelector('.popup-close');
+            overlay = document.querySelector('.popup-consultation'),
+            close = overlay.querySelector('.popup-close');
 
 
         for (let i = 0; i < btns.length; i++) {
@@ -64,6 +64,39 @@ function popups() {
     }
 
     popupConsultation();
+
+     // popup-gift
+    function popupGift() {
+        let btns = document.querySelector('.fixed-gift'),
+            overlay = document.querySelector('.popup-gift'),
+            close = overlay.querySelector('.popup-close');
+
+
+        btns.addEventListener('click', function() {
+            showModal();
+        });
+
+        close.addEventListener('click', function() {
+            closeModal();
+        });
+
+        overlay.addEventListener('click', function() {
+            closeModal();
+        });
+
+        function showModal() {
+            overlay.style.display = 'flex';
+            btns.style.display = 'none';
+            document.body.style.overflow = 'hidden';
+        }
+
+        function closeModal() {
+            overlay.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+    }
+    
+    popupGift();
 
 }
 
