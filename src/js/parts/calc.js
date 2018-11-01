e6e1331695576810c7e5ce5e11e91e256868a5a5
FaZY <FaZY@mail.ru>
2018-11-01 16:29:20 +0300

    Калькулятор

--- src/js/parts/calc.js
+++ src/js/parts/calc.js
@@ -5,32 +5,67 @@ function calc() {
         promo = document.querySelector('.promocode'),
         totalInput = document.querySelector('.calc-price'),
         total = 0,
-        totalValue = 0,
-        sizeValue = 0,
-        materialValue = 0,
-        othersAmenitiesValue = 0,
         promoValue = 'IWANTPOPART';
 
     size.addEventListener('change', function() {
-        totalSize = size[size.selectedIndex].value;
-        console.log(totalSize)
+        totalSize = +this[this.selectedIndex].value;
+
+        if (material[material.selectedIndex].value == 'Выберите материал картины' || size[size.selectedIndex].value == 'Выберите размер картины') {
+            totalInput.textContent = 'Для расчета нужно выбрать размер картины и материал картины'
+        } else {
+            total = totalSize + totalMaterial;
+            totalInput.textContent = total;
+
+            if (promo.value == promoValue) {
+                total = total - ((total / 100)*30);
+                totalInput.textContent = total;
+            }
+        }
         
     });
 
     material.addEventListener('click', function() {
-        totalMaterial = material[material.selectedIndex].value;
-        console.log(totalMaterial)
+        totalMaterial = +this[this.selectedIndex].value;
+
+        if (size[size.selectedIndex].value == 'Выберите размер картины' || material[material.selectedIndex].value == 'Выберите материал картины') {
+            totalInput.textContent = 'Для расчета нужно выбрать размер картины и материал картины'
+        } else {
+            total = totalSize + totalMaterial;
+            totalInput.textContent = total;
+
+            if (promo.value == promoValue) {
+                total = total - ((total / 100)*30);
+                totalInput.textContent = total;
+            }
+        }
     });
 
     othersAmenities.addEventListener('click', function() {
-        totalOthersAmenities = othersAmenities[othersAmenities.selectedIndex].value;
-        console.log(totalOthersAmenities)
+        totalOthersAmenities = +this[this.selectedIndex].value;
+        
+        if (size[size.selectedIndex].value == 'Выберите размер картины' || material[material.selectedIndex].value == 'Выберите материал картины' || othersAmenities[othersAmenities.selectedIndex].value == 'Дополнительные услуги') {
+            totalInput.textContent = total;
+        } else {
+            totalOthers = totalSize + totalMaterial + totalOthersAmenities;
+            totalInput.textContent = totalOthers;
+
+            if (promo.value == promoValue) {
+                totalOthers = totalOthers - ((totalOthers / 100)*30);
+                totalInput.textContent = totalOthers;
+            }
+        }
     });
 
     promo.addEventListener('change', function() {
-        if (promo.value == 'IWANTPOPART') {
-        
+        if (othersAmenities[othersAmenities.selectedIndex].value != 'Дополнительные услуги' && this.value == promoValue) {
+            totalPromo = totalOthers - ((totalOthers / 100)*30);
+            totalInput.textContent = totalPromo;
+        } else if (othersAmenities[othersAmenities.selectedIndex].value != 'Дополнительные услуги' ) {
+            totalInput.textContent = totalOthers;
+        } else {
+            totalInput.textContent = total;
         }
+
     });
 }
 
