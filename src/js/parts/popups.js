0ba7570fc0ad3f5ee04052ae6743e56a836b260d
FaZY <FaZY@mail.ru>
2018-11-01 14:09:11 +0300

    Модальные окна popup-consultation

--- src/js/parts/popups.js
+++ src/js/parts/popups.js
@@ -1,28 +1,70 @@
 function popups() {
-    let btns = document.querySelectorAll('.button-design'),
+    function popupDesign() {
+        let btns = document.querySelectorAll('.button-design'),
         overlay = document.querySelector('.popup-design'),
         close = overlay.querySelector('.popup-close');
     
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', function() {
-            showModal();
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener('click', function() {
+                showModal();
+            });
+        }
+
+        close.addEventListener('click', function() {
+            closeModal();
         });
+
+        overlay.addEventListener('click', function() {
+            closeModal();
+        });
+
+        function showModal() {
+            overlay.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        }
+
+        function closeModal() {
+            overlay.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+    }
+    
+    popupDesign();
+    
+    // button-consultation
+    function popupConsultation() {
+        let btns = document.querySelectorAll('.button-consultation'),
+        overlay = document.querySelector('.popup-consultation'),
+        close = overlay.querySelector('.popup-close');
+
+
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener('click', function() {
+                showModal();
+            });
+        }
+
+        close.addEventListener('click', function() {
+            closeModal();
+        });
+
+        overlay.addEventListener('click', function() {
+            closeModal();
+        });
+
+        function showModal() {
+            overlay.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        }
+
+        function closeModal() {
+            overlay.style.display = 'none';
+            document.body.style.overflow = '';
+        }
     }
 
-    close.addEventListener('click', function() {
-        closeModal();
-    });
+    popupConsultation();
 
-    function showModal() {
-        overlay.style.display = 'flex';
-        document.body.style.overflow = 'hidden';
-    }
-
-    function closeModal() {
-        overlay.style.display = 'none';
-        document.body.style.overflow = '';
-    }
-     
 }
 
 module.exports = popups;
